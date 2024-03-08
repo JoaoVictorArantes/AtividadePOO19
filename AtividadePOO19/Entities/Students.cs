6b675dc9c320953515a0f14dc48cd40f4dba6be1
João Victor <jvaarantes@gmail.com>
2024-03-08 09:16:34 -0300

    Adicionar arquivos de projeto.

---
+++ AtividadePOO19/Entities/Students.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadePOO19.Entities
+{
+    internal class Students
+    {
+        public HashSet<int> ConjuntoDeEstudantesA;
+        public HashSet<int> ConjuntoDeEstudantesB;
+        public HashSet<int> ConjuntoDeEstudantesC;
+
+        public Students(HashSet<int> conjuntoDeEstudantesA, HashSet<int> conjuntoDeEstudantesB, HashSet<int> conjuntoDeEstudantesC)
+        {
+            ConjuntoDeEstudantesA = conjuntoDeEstudantesA;
+            ConjuntoDeEstudantesB = conjuntoDeEstudantesB;
+            ConjuntoDeEstudantesC = conjuntoDeEstudantesC;
+        }
+
+        public int TotalAlunos()
+        { 
+            ConjuntoDeEstudantesA.UnionWith(ConjuntoDeEstudantesB);
+            ConjuntoDeEstudantesA.UnionWith(ConjuntoDeEstudantesC);
+
+            return ConjuntoDeEstudantesA.Count();
+        }
+    }
+}
---
+++ AtividadePOO19/Program.cs
@@ -0,0 +1,54 @@
+﻿using AtividadePOO19.Entities;
+
+namespace AtividadePOO19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> ConjuntoA = new HashSet<int>();
+            HashSet<int> ConjuntoB = new HashSet<int>();
+            HashSet<int> ConjuntoC = new HashSet<int>();
+
+            Console.Write("How many students for course A?  ");
+            int CourseA = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < CourseA; i++)
+            {
+                Console.Write($"Aluno {i + 1}: ");
+
+                int aux = int.Parse(Console.ReadLine());
+
+                ConjuntoA.Add(aux);
+            }
+
+            Console.Write("How many students for course B?  ");
+            int CourseB = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < CourseB; i++)
+            {
+                Console.Write($"Aluno {i + 1}: ");
+
+                int aux = int.Parse(Console.ReadLine());
+
+                ConjuntoB.Add(aux);
+            }
+
+            Console.Write("How many students for course C?  ");
+            int CourseC = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < CourseC; i++)
+            {
+                Console.Write($"Aluno {i + 1}: ");
+
+                int aux = int.Parse(Console.ReadLine());
+
+                ConjuntoC.Add(aux);
+            }
+
+            Students Estudantes = new Students(ConjuntoA, ConjuntoB, ConjuntoC);
+
+            Console.WriteLine($"Total de alunos: {Estudantes.TotalAlunos()}");
+        }
+    }
+}
